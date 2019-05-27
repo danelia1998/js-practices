22620ec37cfaaaaf09490f4540dc8536682432c7
test <davit.danelia.1@btu.edu.ge>
2019-05-27 21:53:51 +0400

    feat(01-Variables-Data-Types-Basic-Operators): Finish task(1,2,3)

---
+++ 01-Variables-Data-Types-Basic-Operators/task-2.js
@@ -0,0 +1,4 @@
+var login = 'Owner'
+
+var message = (login == 'Pitter') ? 'hi' : (login == 'Owner') ? 'Hello' : (login == '') ? 'unknown' :'';
+console.log(message);
---
+++ 01-Variables-Data-Types-Basic-Operators/task-3.js
@@ -0,0 +1,12 @@
+var val = 'e'
+
+if (val == 'a') {
+    console.log('a');
+}else if (val == 'b'){
+}else if (val == 'c'){
+}else if (val == 'd'){
+}else if (val == 'e'){
+    console.log('others');
+}else {
+    console.log('unknown');
+}
