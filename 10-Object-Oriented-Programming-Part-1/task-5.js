d48022c33d7e0210d1699463feb491a8b45cc0f0
test <davit.danelia.1@btu.edu.ge>
2019-06-07 21:29:08 +0400

    fix(Fixing bugs): Finish task

--- 10-Object-Oriented-Programming-Part-1/task-5.js
+++ 10-Object-Oriented-Programming-Part-1/task-5.js
@@ -1,12 +1,12 @@
 function CoffeeMachine(power, capacity) {
     var waterAmount = 0;
     var WATER_HEAT_CAPACITY = 4200;
-    this.running = false;
+    let timerid = false;
 
     function getTimeToBoil() {
         return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
     }
- 
+
     this.setWaterAmount = function (amount) {
         if (amount < 0) {
             throw new Error("Value has to be positive");
@@ -16,36 +16,36 @@ function CoffeeMachine(power, capacity) {
         }
         waterAmount = amount;
     };
- 
+
     function onReady() {
         console.log('Coffee is ready');
     }
- 
 
     this.run = function () {
-        setTimeout(onReady, getTimeToBoil());
-        this.running = true;
+        timerid = setTimeout(function () {
+            timerid = false;
+            onReady();
+        }, getTimeToBoil());
     };
- 
-    this.setOnReady = function (func) {
-        this.running = false;
-        return func();
-    }
- 
-    this.isRunning = function () {
-        return this.running
-    }
- }
- 
- var coffeeMachine = new CoffeeMachine(20000, 500);
- coffeeMachine.setWaterAmount(100);
 
- console.log('Before: ' + coffeeMachine.isRunning());
- 
- coffeeMachine.run();
- 
-//  console.log('In progress: ' + coffeeMachine.isRunning());
- 
-//  coffeeMachine.setOnReady(function() {
-//      console.log('After: ' + coffeeMachine.isRunning());
-//  });
+    this.setOnReady = function (func) {
+        onReady = func;
+    }
+
+    this.isRunning = function () {
+        return !!timerid;
+    }
+}
+
+var coffeeMachine = new CoffeeMachine(20000, 500);
+coffeeMachine.setWaterAmount(100);
+
+console.log('Before: ' + coffeeMachine.isRunning()); // Before: false
+
+coffeeMachine.run();
+
+console.log('In progress: ' + coffeeMachine.isRunning()); // In progress: true
+
+coffeeMachine.setOnReady(function () {
+    console.log('After: ' + coffeeMachine.isRunning()); // After: false
+});
